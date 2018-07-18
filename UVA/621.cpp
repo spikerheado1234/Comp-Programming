e4c8f7b6dda2d7c8a7300b2ddfd576e0a82d9ae7
spikerheado1234 <ahangupta.96@gmail.com>
2018-07-18 18:47:59 +0800

    Solution to 621 uva problem

--- UVA/621.cpp
+++ UVA/621.cpp
@@ -10,17 +10,19 @@ string notCompleted = "?";
 bool isMatching(int firstDigits, int numberToCheck) {
   int numberFirstDigits = 1;
   int firstDigitsCopy = firstDigits;
-  while (firstDigitsCopy >= 10) {
+  while (firstDigitsCopy > 10) {
     numberFirstDigits++;
     firstDigitsCopy = firstDigitsCopy / 10;
   }
-
   int numberToCheckCopy = numberToCheck;
-  int frontNumber = numberFirstDigits * 10;
-  while (numberToCheck >= frontNumber) {
+  int frontNumber = 1;
+  for (int i = 0; i < numberFirstDigits; i++) {
+    frontNumber = frontNumber * 10;
+  }
+  while (numberToCheckCopy >= frontNumber) {
     numberToCheckCopy = numberToCheckCopy / 10;
   }
-  return firstDigitsCopy == numberToCheckCopy;
+  return firstDigits == numberToCheckCopy;
 }
 
 
@@ -30,26 +32,28 @@ int main() {
   for (int i = 0; i < numberQueries; i++) {
     int currentQuery;
     cin >> currentQuery;
-    if (currentQuery % 100 == 35) { // Possible negative result.
+    if ((currentQuery == 1) || (currentQuery == 4) || (currentQuery == 78)) {
+      cout << positiveResult << endl;
+    } else if (currentQuery%100 == 35) { // Possible negative result.
       if (currentQuery == 135 || currentQuery == 435 || currentQuery == 7835) {
-        cout << negativeResult;
+        cout << negativeResult << endl;
       } else {
-        cout << positiveResult;
+        cout << positiveResult << endl;
       }
     } else if ((currentQuery % 10 == 4) && isMatching(9, currentQuery)) { // Possible experiment failed.
       if (currentQuery == 914 || currentQuery == 944 || currentQuery == 9784) {
-        cout << failedResult;
+        cout << failedResult << endl;
       } else {
-        cout << positiveResult;
+        cout << positiveResult << endl;
       }
     } else if (isMatching(190, currentQuery)) { // Possible experiment not completed.
       if (currentQuery == 1901 || currentQuery == 1904 || currentQuery == 19078) {
-        cout << notCompleted;
+        cout << notCompleted << endl;
       } else {
-        cout << positiveResult;
+        cout << positiveResult << endl;
       }
-    } else { // Fucked if we reach here anyways.
-      cout << "Error encountered";
+    } else {
+      cout << positiveResult << endl;
     }
   }
 }
